1c41e2664f629134d1600e04ef45d982ba74abd8
och9854 <och9854@naver.com>
2022-09-16 13:45:47 -0700

    last

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"edu.sjsu.assignment2","l":"MyFile"},{"p":"edu.sjsu.assignment2","l":"ScanMyTime"}];updateSearchResults();
